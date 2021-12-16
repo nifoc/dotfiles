5b6fc286219cae5d58fcb89a2088fa899394e569
Daniel Kempkens <daniel+git@kempkens.io>
2021-12-16 21:12:34 +0100

    Update bufferline config

--- config/nvim/lua/nifoc/config/bufferline.lua
+++ config/nvim/lua/nifoc/config/bufferline.lua
@@ -1,5 +1,23 @@
 require('bufferline').setup{
   options = {
     show_close_icon = false,
+
+    diagnostics = "nvim_lsp",
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      if context.buffer:current() then
+        return ''
+      end
+
+      return " (" .. count .. ")"
+    end,
+
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = "File Explorer",
+        highlight = "Directory",
+        text_align = "left",
+      },
+    },
   }
 }
