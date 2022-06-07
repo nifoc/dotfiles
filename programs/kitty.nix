43b6d6ebb871524420e150af8bdf6b591b20593a
Daniel Kempkens <daniel+git@kempkens.io>
2022-06-07 19:50:18 +0200

    kitty: Update tab style

--- programs/kitty.nix
+++ programs/kitty.nix
@@ -13,25 +13,29 @@
       window_border_width = "1.0";
       window_margin_width = "0.0";
       window_padding_width = "1 3 1 3";
-      inactive_text_alpha = "0.8";
       macos_titlebar_color = "background";
       macos_traditional_fullscreen = true;
       macos_show_window_title_in = "window";
       enabled_layouts = "tall:bias=55;tall:bias=50;full_size=1;stack;fat;grid;horizontal;split;mirrored=false;";
 
       # Tabs
-      tab_bar_style = "custom";
-      tab_separator = "\"\"";
+      tab_bar_style = "separator";
+      tab_separator = "\"  \"";
       tab_fade = "0 0 0 0";
-      tab_activity_symbol = "none";
+      tab_activity_symbol = "\" ●\"";
       bell_on_tab = false;
       tab_bar_edge = "bottom";
+      tab_bar_background = "#282A36";
       tab_bar_margin_height = "0.0 0.0";
+      inactive_tab_foreground = "#F8F8F2";
+      inactive_tab_background = "#282A36";
       inactive_tab_font_style = "normal";
-      active_tab_font_style = "bold-italic";
+      active_tab_foreground = "#F8F8F2";
+      active_tab_background = "#282A36";
+      active_tab_font_style = "normal";
       tab_bar_min_tabs = 2;
-      tab_title_template = "\"{fmt.fg._6272a4}{fmt.bg.default} ○ {index}:{f'{title[:6]}…{title[-6:]}' if title.rindex(title[-1]) + 1 > 13 else title}{' []' if layout_name == 'stack' else ''} \"";
-      active_tab_title_template = "\"{fmt.fg._f8f8f2}{fmt.bg.default} 綠{index}:{f'{title[:6]}…{title[-6:]}' if title.rindex(title[-1]) + 1 > 13 else title}{' []' if layout_name == 'stack' else ''} \"";
+      tab_title_template = "\" {index}: {title[:30]}{fmt.fg._FF5555}{activity_symbol}\"";
+      active_tab_title_template = "\" {index}: {title[:30]}\"";
 
       # UX
       kitty_mod = "cmd";
