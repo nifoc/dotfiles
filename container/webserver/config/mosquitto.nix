e7db9d3f69a6c7ff7f31d7e186c2e2d8c6af5424
Daniel Kempkens <daniel+git@kempkens.io>
2022-07-31 00:44:20 +0200

    webserver: Add mosquitto config

--- container/webserver/config/mosquitto.nix
+++ container/webserver/config/mosquitto.nix
@@ -1,6 +1,11 @@
+let
+  secret = import ../../secret/container/webserver.nix;
+in
 {
   environment.usr."local/etc/container-webserver/mosquitto/mosquitto.conf".text = ''
     listener 1883
     password_file /mosquitto/config/users.conf
   '';
+
+  environment.usr."local/etc/container-webserver/mosquitto/users.conf".text = secret.container.webserver.mosquitto.users;
 }
