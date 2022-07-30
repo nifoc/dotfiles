33e742804a4aaf23946efd23ddc2ec34e6cbb4e7
Daniel Kempkens <daniel+git@kempkens.io>
2022-07-30 21:38:01 +0200

    Use arion overlay

--- system/flakes/sail.nix
+++ system/flakes/sail.nix
@@ -1,8 +1,23 @@
 { nixpkgs, arion, ... }:
 
+let
+  overlay-arion = arion.overlay;
+
+  nixpkgsConfig = {
+    overlays = [
+      overlay-arion
+    ];
+
+    config = {
+      allowUnfree = true;
+      allowBroken = true;
+    };
+  };
+in
 {
   nixosConfigurations.sail = nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
+    nixpkgs = nixpkgsConfig;
     modules = [
       arion.nixosModules.arion
 
