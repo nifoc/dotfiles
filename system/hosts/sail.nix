088ab6419b7c4ca6a43c759b5b174e2ce9a55382
Daniel Kempkens <daniel+git@kempkens.io>
2022-07-30 22:55:19 +0200

    Add IPv6 config

--- system/hosts/sail.nix
+++ system/hosts/sail.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 
+let
+  secret = import ../../secret/hosts/sail.nix;
+in
 {
   imports = [
     ../../hardware/hosts/sail.nix
@@ -34,7 +37,12 @@
 
   zramSwap.enable = true;
 
-  networking.hostName = "sail";
+  networking = {
+    hostName = "sail";
+
+    interfaces.enp1s0.ipv6.addresses = secret.networking.interfaces.enp1s0.ipv6.addresses;
+    defaultGateway6 = { address = "fe80::1"; interface = "enp1s0"; };
+  };
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDTRKrX0wVWoauI+dSfFpKIr7qwh8imrp1OVlxWyCuFssZI2o6chmRBZq4EbOkVvfdgdOGJ4hTSwQOTwusZVrZHwxtinusX5c2M3HMrhMvLFAYh2vKrvP5UZBTA/7laenSM6uLGxrM2CNc+G8ryAyY7dLoU2xaXiqgcfC6MaU/leTMTXWH5fHvqUk2pxI4ihObTGiDE5kvvFbv3AhFFtcMmkzdHN1vlVp8olKG0lYOLM9O3PpVGJtiOTA2Z9YuTC44iH984HPmk3KkJsK/PZFI1d4K7oW7298w44pszElGwjIW0GyB7X4qo6yOxsq1OsL/c7T9VHWVw3na2QvAJODBNM8rYGMACo8oSEThRsi1hhXPe7MhLPqwhn3OHbbofupbqk7WpKVaMLrO7793DgNatPNhGgbz9WtVCC65YPY3De3sowsh4M4u0HC5lRg0IgjBMgUjMAHWqcxytYRUKPRrdKbcaoOoMquMnja+bcLfHxVOkcLMgEfzPtjxgZCvenUsy0Q73dwxAtJx73xscMZUWb/LbkLWoOzAh3jjyjIeZeT8ZyIOhfcgoxLFE4Ik7eIiszEg0xQ7jXzR+K3RL+2O93RvNTvc5WaDVypiL6Zics30EZAYMYtBoY69a1PxeJ42sLp3O35QNFbwfN4cnsx9YXgvUmTcwCijU/LbvNAgl2Q=="
