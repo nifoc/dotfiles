ca818ed4cef0d36f29d2a760809d56b6ffa3dd5c
Daniel Kempkens <daniel+git@kempkens.io>
2022-08-22 21:15:44 +0200

    Styx: Adjust to nix-darwin updates

--- system/hosts/Styx.nix
+++ system/hosts/Styx.nix
@@ -12,31 +12,32 @@ in
   ];
 
   nix = {
-    binaryCaches = [
-      "https://nix-community.cachix.org"
-      "https://nifoc.cachix.org"
-    ] ++ optionals secret.nix-cache.nifoc.enabled [
-      secret.nix-cache.nifoc.s3Url
-    ];
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      extra-platforms = [ "x86_64-darwin" ];
 
-    binaryCachePublicKeys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nifoc.cachix.org-1:ymuftq7RgN/lf/iWXFK8gpwDSAGFaGBeliWe9u6q8II="
-    ] ++ optionals secret.nix-cache.nifoc.enabled [
-      secret.nix-cache.nifoc.publicKeyValue
-    ];
+      auto-optimise-store = true;
+      keep-derivations = true;
+      keep-outputs = true;
 
-    trustedUsers = [
-      "@admin"
-    ];
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://nifoc.cachix.org"
+      ] ++ optionals secret.nix-cache.nifoc.enabled [
+        secret.nix-cache.nifoc.s3Url
+      ];
 
-    extraOptions = ''
-      experimental-features = nix-command flakes
-      extra-platforms = x86_64-darwin
-      keep-derivations = true
-      keep-outputs = true
-      auto-optimise-store = true
-    '';
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nifoc.cachix.org-1:ymuftq7RgN/lf/iWXFK8gpwDSAGFaGBeliWe9u6q8II="
+      ] ++ optionals secret.nix-cache.nifoc.enabled [
+        secret.nix-cache.nifoc.publicKeyValue
+      ];
+
+      trusted-users = [ "@admin" ];
+    };
+
+    configureBuildUsers = true;
 
     distributedBuilds = true;
 
@@ -50,8 +51,6 @@ in
   };
 
   users = {
-    nix.configureBuildUsers = true;
-
     users.daniel = {
       home = "/Users/daniel";
     };
