3b675768a95949807476445aa830efe6405e79df
Daniel Kempkens <daniel+git@kempkens.io>
2022-11-11 15:29:35 +0100

    Synapse: Fix permissions

--- container/matrix/config.nix
+++ container/matrix/config.nix
@@ -4,13 +4,13 @@
   # Synapse
 
   systemd.tmpfiles.rules = [
-    "d /etc/container-matrix/synapse 0755 0 0"
+    "d /etc/container-matrix/synapse 0755 991 991"
   ];
 
   environment.etc."container-matrix/synapse/homeserver.yaml" = {
     source = ../../secret/container/matrix/config/homeserver.yaml;
     mode = "0640";
-    uid = 0;
-    gid = 0;
+    uid = 991;
+    gid = 991;
   };
 }
