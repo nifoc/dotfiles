0fe049ad365c111253553fffd508773b5745575d
Daniel Kempkens <daniel+git@kempkens.io>
2023-02-13 19:48:41 +0100

    nitter: Remove from webserver config

--- container/webserver/default.nix
+++ container/webserver/default.nix
@@ -37,24 +37,6 @@ in
           inherit (secret.container.webserver.weewx) labels;
         };
       };
-
-      nitter = {
-        service = {
-          image = "zedeus/nitter:latest";
-          container_name = "nitter";
-          restart = "unless-stopped";
-          ports = [ "127.0.0.1:8001:8080" ];
-          environment = {
-            "TZ" = "Europe/Berlin";
-          };
-          volumes = [
-            "/etc/container-webserver/nitter/nitter.conf:/src/nitter.conf"
-          ];
-          labels = {
-            "com.centurylinklabs.watchtower.enable" = "true";
-          };
-        };
-      };
     };
   };
 } // custom-config
