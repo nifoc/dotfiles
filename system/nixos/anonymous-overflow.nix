bb911d884270fc9a9de502d85c84fcc366566db2
Daniel Kempkens <daniel+git@kempkens.io>
2023-02-15 22:04:11 +0100

    anonymous-overflow: Include static files

--- system/nixos/anonymous-overflow.nix
+++ system/nixos/anonymous-overflow.nix
@@ -12,6 +12,7 @@ in
       DynamicUser = true;
       StateDirectory = "anonymous-overflow";
       EnvironmentFile = [ config.age.secrets.anonymous-overflow-config.path ];
+      WorkingDirectory = "${anonymous-overflow-pkg}/share/anonymous-overflow";
       ExecStart = "${anonymous-overflow-pkg}/bin/anonymousoverflow";
       Restart = "on-failure";
       RestartSec = "5s";
@@ -28,11 +29,16 @@ in
         }
       ];
 
+      root = "${anonymous-overflow-pkg}/share/anonymous-overflow/public/";
       forceSSL = false;
       enableACME = false;
+      basicAuthFile = config.age.secrets.anonymous-overflow-auth.path;
 
       locations."/" = {
-        basicAuthFile = config.age.secrets.anonymous-overflow-auth.path;
+        tryFiles = "$uri @proxy";
+      };
+
+      locations."@proxy" = {
         recommendedProxySettings = true;
         proxyPass = "http://127.0.0.1:8003";
       };
