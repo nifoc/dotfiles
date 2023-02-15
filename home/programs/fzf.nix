9f3a0e3afb24a0256bd045835d605458e7eb17e2
Daniel Kempkens <daniel+git@kempkens.io>
2023-02-16 00:33:23 +0100

    fzf: Dracula!

--- home/programs/fzf.nix
+++ home/programs/fzf.nix
@@ -7,5 +7,21 @@
     defaultCommand = "fd --type file --hidden --no-ignore";
     fileWidgetCommand = "fd --type file --hidden --no-ignore";
     changeDirWidgetCommand = "fd --type directory --hidden --no-ignore";
+
+    colors = {
+      fg = "#f8f8f2";
+      "fg+" = "#f8f8f2";
+      bg = "#282a36";
+      "bg+" = "#44475a";
+      hl = "#bd93f9";
+      "hl+" = "#bd93f9";
+
+      info = "#ffb86c";
+      prompt = "#50fa7b";
+      pointer = "#ff79c6";
+      marker = "#ff79c6";
+      spinner = "#ffb86c";
+      header = "#6272a4";
+    };
   };
 }
