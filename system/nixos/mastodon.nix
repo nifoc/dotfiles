6adbb3bbda91662caf6921511dd14965d5003c4c
Daniel Kempkens <daniel+git@kempkens.io>
2023-03-06 23:33:55 +0100

    sail: Try setting STS headers for SSL only

--- system/nixos/mastodon.nix
+++ system/nixos/mastodon.nix
@@ -117,10 +117,7 @@ in
 
     extraConfig = ''
       add_header Access-Control-Allow-Origin https://mastodon.kempkens.io;
-
-      if ($ssl_protocol != "") {
-        add_header Strict-Transport-Security max-age=31536000\;\ includeSubDomains\;\ preload;
-      }
+      add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
     '';
 
     locations."/system/".alias = "/var/lib/mastodon/public-system/";
