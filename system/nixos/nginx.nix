dfcc5d6f8fdf0293a1f232a13092bfa389db1063
Daniel Kempkens <daniel+git@kempkens.io>
2023-03-06 10:45:35 +0100

    sail: Open HTTP3 FW port

--- system/nixos/nginx.nix
+++ system/nixos/nginx.nix
@@ -14,9 +14,12 @@
   networking.firewall.interfaces =
     let
       nginxTCPPorts = [ 80 443 ];
+      nginxUDPPorts = [ 443 ];
     in
     {
       "enp1s0".allowedTCPPorts = nginxTCPPorts;
+      "enp1s0".allowedUDPPorts = nginxUDPPorts;
       "tailscale0".allowedTCPPorts = nginxTCPPorts;
+      "tailscale0".allowedUDPPorts = nginxUDPPorts;
     };
 }
