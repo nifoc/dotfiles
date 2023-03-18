eb9404d83c19f7d7452b797e996ab8773e985133
Daniel Kempkens <daniel+git@kempkens.io>
2023-03-19 00:28:41 +0100

    proxitok: Switch to oci-containers config

--- container/proxitok/default.nix
+++ container/proxitok/default.nix
@@ -1,37 +1,27 @@
 { config, ... }:
 
 {
-  virtualisation.arion.projects.proxitok.settings = {
-    services = {
-      proxitok-web = {
-        service = {
-          image = "ghcr.io/pablouser1/proxitok:master";
-          container_name = "proxitok-web";
-          restart = "unless-stopped";
-          depends_on = [ "proxitok-signer" ];
-          ports = [ "127.0.0.1:8005:80" ];
-          env_file = [ config.age.secrets.proxitok-environment.path ];
-          volumes = [
-            "/etc/container-proxitok/cache:/cache"
-          ];
-          labels = {
-            "com.centurylinklabs.watchtower.enable" = "true";
-            "io.containers.autoupdate" = "registry";
-          };
-        };
-      };
+  virtualisation.oci-containers.containers = {
+    proxitok-web = {
+      image = "ghcr.io/pablouser1/proxitok:master";
+      dependsOn = [ "proxitok-signer" ];
+      ports = [ "127.0.0.1:8005:80" ];
+      environmentFiles = [ config.age.secrets.proxitok-environment.path ];
+      volumes = [
+        "/etc/container-proxitok/cache:/cache"
+      ];
+      extraOptions = [
+        "--label=com.centurylinklabs.watchtower.enable=true"
+        "--label=io.containers.autoupdate=registry"
+      ];
+    };
 
-      proxitok-signer = {
-        service = {
-          image = "ghcr.io/pablouser1/signtok:master";
-          container_name = "proxitok-signer";
-          restart = "unless-stopped";
-          labels = {
-            "com.centurylinklabs.watchtower.enable" = "true";
-            "io.containers.autoupdate" = "registry";
-          };
-        };
-      };
+    proxitok-signer = {
+      image = "ghcr.io/pablouser1/signtok:master";
+      extraOptions = [
+        "--label=com.centurylinklabs.watchtower.enable=true"
+        "--label=io.containers.autoupdate=registry"
+      ];
     };
   };
 
