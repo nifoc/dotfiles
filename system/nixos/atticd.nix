2955832f15a6209b111130e0fd007079023de0aa
Daniel Kempkens <daniel+git@kempkens.io>
2023-04-05 12:22:05 +0200

    atticd: use postgres

--- system/nixos/atticd.nix
+++ system/nixos/atticd.nix
@@ -13,7 +13,7 @@ in
 
     settings = {
       listen = "127.0.0.1:8080";
-      database.url = "";
+      database.url = secret.atticd.database-url;
 
       allowed-hosts = [ "${fqdn}" ];
       api-endpoint = "https://${fqdn}/";
