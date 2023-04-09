fbbed3f77a62c53cef989b85ae7290421c932683
Daniel Kempkens <daniel+git@kempkens.io>
2023-04-09 02:05:16 +0200

    fixup

--- system/nixos/home-proxy.nix
+++ system/nixos/home-proxy.nix
@@ -5,9 +5,7 @@
     resolver 1.1.1.1 ipv6=off;
 
     server {
-      listen 0.0.0.0:${builtins.toString(secret.nginx.upstream.home.port)} proxy_protocol;
-      proxy_connect_timeout 2s;
-      proxy_timeout 3s;
+      listen 0.0.0.0:${builtins.toString(secret.nginx.upstream.home.port)};
       proxy_pass ${secret.nginx.upstream.home.hostname}:${builtins.toString(secret.nginx.upstream.home.port)};
     }
   '';
