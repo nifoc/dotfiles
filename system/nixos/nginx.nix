4841a89c3d52ac392d22ebdc82c5c6c7c13fa27f
Daniel Kempkens <daniel+git@kempkens.io>
2023-04-14 16:21:39 +0200

    mediaserver: add nginx

--- system/nixos/nginx.nix
+++ system/nixos/nginx.nix
@@ -1,8 +1,5 @@
 { pkgs, lib, config, ... }:
 
-let
-  interfaces = lib.mapAttrsToList (name: value: value.matchConfig.Name) config.systemd.network.networks ++ [ "tailscale0" ];
-in
 {
   services.nginx = {
     enable = true;
@@ -29,15 +26,19 @@ in
     '';
   };
 
-  networking.firewall.interfaces = builtins.listToAttrs
-    (builtins.map
-      (iface:
-        {
-          name = iface;
-          value = {
-            allowedTCPPorts = [ 80 443 ];
-            allowedUDPPorts = [ 443 ];
-          };
-        })
-      (builtins.filter builtins.isString interfaces));
+  networking.firewall.interfaces =
+    let
+      interfaces = lib.mapAttrsToList (_: value: value.matchConfig.Name ? null) config.systemd.network.networks ++ [ "tailscale0" ];
+    in
+    builtins.listToAttrs
+      (builtins.map
+        (iface:
+          {
+            name = iface;
+            value = {
+              allowedTCPPorts = [ 80 443 ];
+              allowedUDPPorts = [ 443 ];
+            };
+          })
+        (builtins.filter builtins.isString interfaces));
 }
