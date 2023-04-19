e8294c41142317ad7c7c8178873f168dfcbbe9ca
Daniel Kempkens <daniel+git@kempkens.io>
2023-04-19 22:14:39 +0200

    mediaserver: wip jellyfin

--- system/nixos/jellyfin.nix
+++ system/nixos/jellyfin.nix
@@ -9,19 +9,34 @@
   };
 
   services.nginx.virtualHosts."jellyfin.internal.kempkens.network" = {
-    listen = [{
-      addr = "0.0.0.0";
-      port = 9920;
-      ssl = true;
-    }];
+    listen = [
+      {
+        addr = "0.0.0.0";
+        port = 9920;
+        ssl = true;
+      }
+
+      {
+        addr = "[::0]";
+        port = 9920;
+        ssl = true;
+      }
+    ];
 
     quic = true;
     http3 = true;
+
+    serverAliases = [ "jellyfin.home.kempkens.io" ];
 
     onlySSL = true;
     useACMEHost = "internal.kempkens.network";
 
     locations."/" = {
+      recommendedProxySettings = true;
+      proxyPass = "http://127.0.0.1:8096";
+    };
+
+    locations."/socket" = {
       recommendedProxySettings = true;
       proxyPass = "http://127.0.0.1:8096";
       proxyWebsockets = true;
