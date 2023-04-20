ff39cbc56c92e8c39b523bb2ef546ad0d4d7c8ed
Daniel Kempkens <daniel+git@kempkens.io>
2023-04-20 15:12:26 +0200

    mediaserver: wip tubearchivist

--- container/tubearchivist/default.nix
+++ container/tubearchivist/default.nix
@@ -7,32 +7,32 @@
     "d /var/lib/tubearchivist/es 0755 root root"
   ];
 
-  # virtualisation.oci-containers.containers = {
-  #   tubearchivist = {
-  #     image = "docker.io/bbilly1/tubearchivist:latest";
-  #     dependsOn = [ "archivist-es" "archivist-redis" ];
-  #     ports = [ "127.0.0.1:9887:8000" ];
-  #     environmentFiles = [ config.age.secrets.tubearchivist-environment-ta.path ];
-  #     volumes = [
-  #       "/mnt/media/YTDL/Downloads:/youtube"
-  #       "/mnt/media/YTDL/Cache:/cache"
-  #     ];
-  #   };
-  #
-  #   archivist-redis = {
-  #     image = "docker.io/redis/redis-stack-server:latest";
-  #     volumes = [
-  #       "/var/lib/tubearchivist/redis:/data"
-  #     ];
-  #   };
-  #
-  #   archivist-es = {
-  #     image = "docker.io/bbilly1/tubearchivist-es:latest";
-  #     environmentFiles = [ config.age.secrets.tubearchivist-environment-es.path ];
-  #     extraOptions = [ "--ulimit memlock=-1:-1" ];
-  #     volumes = [
-  #       "/var/lib/tubearchivist/es:/usr/share/elasticsearch/data"
-  #     ];
-  #   };
-  # };
+  virtualisation.oci-containers.containers = {
+    #   tubearchivist = {
+    #     image = "docker.io/bbilly1/tubearchivist:latest";
+    #     dependsOn = [ "archivist-es" "archivist-redis" ];
+    #     ports = [ "127.0.0.1:9887:8000" ];
+    #     environmentFiles = [ config.age.secrets.tubearchivist-environment-ta.path ];
+    #     volumes = [
+    #       "/mnt/media/YTDL/Downloads:/youtube"
+    #       "/mnt/media/YTDL/Cache:/cache"
+    #     ];
+    #   };
+    #
+    archivist-redis = {
+      image = "docker.io/redis/redis-stack-server:latest";
+      volumes = [
+        "/var/lib/tubearchivist/redis:/data"
+      ];
+    };
+
+    archivist-es = {
+      image = "docker.io/bbilly1/tubearchivist-es:latest";
+      environmentFiles = [ config.age.secrets.tubearchivist-environment-es.path ];
+      extraOptions = [ "--ulimit memlock=-1:-1" ];
+      volumes = [
+        "/var/lib/tubearchivist/es:/usr/share/elasticsearch/data"
+      ];
+    };
+  };
 }
