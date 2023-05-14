c0eecd0fb396b27ffb8d382d24829017a22b7d28
Daniel Kempkens <daniel+git@kempkens.io>
2023-05-14 12:55:29 +0200

    hw: set TCP sysctls

--- hardware/hosts/sail.nix
+++ hardware/hosts/sail.nix
@@ -17,6 +17,9 @@
     kernel.sysctl = {
       "net.core.default_qdisc" = "fq";
       "net.ipv4.tcp_congestion_control" = "bbr";
+      "net.ipv4.tcp_syncookies" = 0;
+      "net.ipv4.tcp_timestamps" = 1;
+      "net.ipv4.tcp_window_scaling" = 1;
       "net.core.rmem_max" = 2500000;
     };
   };
