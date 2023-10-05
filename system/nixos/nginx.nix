aa7e3b4fc86454a48004997171e9f96da51e250b
Daniel Kempkens <daniel+git@kempkens.io>
2023-10-05 13:13:12 +0200

    nginx: enable KTLS

--- system/nixos/nginx.nix
+++ system/nixos/nginx.nix
@@ -3,7 +3,9 @@
 {
   services.nginx = {
     enable = true;
-    package = pkgs.nginxQuic;
+    package = pkgs.nginxQuic.override {
+      withKTLS = true;
+    };
 
     recommendedOptimisation = true;
     recommendedGzipSettings = true;
