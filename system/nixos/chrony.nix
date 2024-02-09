305b3a7d549a88fc7f5443e2057d45b7ae4b2195
Daniel Kempkens <daniel+git@kempkens.io>
2024-02-09 10:01:11 +0100

    chrony: update config

--- system/nixos/chrony.nix
+++ system/nixos/chrony.nix
@@ -8,9 +8,9 @@
       "ptbtime1.ptb.de"
       "ptbtime2.ptb.de"
       "ptbtime3.ptb.de"
-      "ntp1.rwth-aachen.de"
-      "ntp2.rwth-aachen.de"
-      "ntp3.rwth-aachen.de"
+      "134.130.4.17" # "ntp1.rwth-aachen.de"
+      "134.130.5.17" # "ntp2.rwth-aachen.de"
+      "137.226.119.25" # "ntp3.rwth-aachen.de"
       "ntp1.hetzner.de"
       "ntp2.hetzner.de"
       "ntp3.hetzner.de"
@@ -18,6 +18,7 @@
     ];
 
     extraConfig = ''
+      burst 3/3
       makestep 1.0 3
 
       bindaddress 0.0.0.0
