bf3bde8e4aed2f208c48c74c22a0daf4df9f292a
Daniel Kempkens <daniel+git@kempkens.io>
2024-02-16 00:14:41 +0100

    nvim: update ts parsers

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -403,5 +403,10 @@ in
       $VERBOSE_ECHO "Resetting loader"
       $DRY_RUN_CMD ${lib.getExe config.programs.neovim.finalPackage} -l <(echo "vim.loader.reset()")
     '';
+
+    activation.neovimTreeSitter = lib.hm.dag.entryAfter [ "writeBoundary" ] /* bash */ ''
+      $VERBOSE_ECHO "Updating tree-sitter parsers"
+      $DRY_RUN_CMD ${lib.getExe config.programs.neovim.finalPackage} -c 'TSUpdateSync | q' --headless
+    '';
   };
 }
