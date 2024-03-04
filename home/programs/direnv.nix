d37e378b004529c833cb8fcfeaceece7155a5c8c
Daniel Kempkens <daniel+git@kempkens.io>
2024-03-04 10:37:45 +0100

    direnv: use new 2.34 features

--- home/programs/direnv.nix
+++ home/programs/direnv.nix
@@ -3,6 +3,12 @@
     enable = true;
     nix-direnv.enable = true;
 
+    config = {
+      strict_env = true;
+      warn_timeout = "1m";
+      hide_env_diff = true;
+    };
+
     stdlib = builtins.readFile ../config/direnv/direnvrc;
   };
 }
