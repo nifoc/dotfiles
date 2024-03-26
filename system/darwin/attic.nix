3002f43f7fd25fe94b17846cf1e815eaad07324b
Daniel Kempkens <daniel+git@kempkens.io>
2024-03-26 20:17:09 +0100

    attic: remove overrides

--- system/darwin/attic.nix
+++ system/darwin/attic.nix
@@ -1,14 +1,5 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
-let
-  inherit (pkgs) stdenv;
-
-  attic = pkgs.attic-client.overrideAttrs (_: _: {
-    env = lib.optionalAttrs stdenv.cc.isClang {
-      NIX_LDFLAGS = "-l${stdenv.cc.libcxx.cxxabi.libName}";
-    };
-  });
-in
 {
-  environment.systemPackages = [ attic ];
+  environment.systemPackages = [ pkgs.attic-client ];
 }
