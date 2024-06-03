f23a52224da4a476b6690688c6de9ab70f8098fc
Daniel Kempkens <daniel+git@kempkens.io>
2024-06-03 20:24:06 +0200

    tubearchivist: update

--- container/tubearchivist/default.nix
+++ container/tubearchivist/default.nix
@@ -10,7 +10,7 @@
 
   virtualisation.oci-containers.containers = {
     tubearchivist = {
-      image = "docker.io/bbilly1/tubearchivist:v0.4.7";
+      image = "docker.io/bbilly1/tubearchivist:v0.4.8";
       dependsOn = [ "archivist-es" "archivist-redis" ];
       ports = [ "127.0.0.1:9887:8000" ];
       environmentFiles = [ config.age.secrets.tubearchivist-environment-ta.path ];
