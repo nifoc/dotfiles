c88393134cccf260ced19f4ccebb60d5d96e758e
Daniel Kempkens <daniel+git@kempkens.io>
2024-10-21 15:53:15 +0200

    Update deps

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     # Lix
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
