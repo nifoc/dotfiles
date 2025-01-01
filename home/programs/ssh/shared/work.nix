d52d54d5aa609e208a1c82fc9fd460350708a136
Daniel Kempkens <daniel+git@kempkens.io>
2025-01-02 00:22:27 +0100

    ssh: cleanup

--- home/programs/ssh/shared/work.nix
+++ home/programs/ssh/shared/work.nix
@@ -10,38 +10,11 @@
       compression = true;
     };
 
-    "nedeco-smartos-hosting" = {
-      host = "10.0.90.*";
-      port = 22;
-      user = "root";
-      identityFile = "~/.ssh/nedeco.pub";
-      identitiesOnly = true;
-    };
-
     "msc.nedeco.local" = {
       port = 22;
       user = "root";
       identityFile = "~/.ssh/nedeco.pub";
       identitiesOnly = true;
-    };
-
-    "headnode.nedeco-hosting.local" = {
-      hostname = "10.0.90.13";
-      port = 22;
-      user = "root";
-      extraOptions = {
-        PreferredAuthentications = "password";
-      };
-    };
-
-    "cnode01.nedeco-hosting.local" = {
-      hostname = "10.0.88.45";
-      port = 22;
-      user = "root";
-      proxyCommand = "ssh headnode.nedeco-hosting.local nc -w 120 %h %p";
-      extraOptions = {
-        PreferredAuthentications = "password";
-      };
     };
 
     "192.168.8.141" = {
