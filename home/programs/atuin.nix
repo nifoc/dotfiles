135eddd411292eb5286b55ab3d7bc1a89baf5d1c
Daniel Kempkens <daniel+git@kempkens.io>
2025-01-31 23:08:48 +0100

    atuin: set logLevel for daemon

--- home/programs/atuin.nix
+++ home/programs/atuin.nix
@@ -1,41 +1,44 @@
 {
-  programs.atuin = {
-    enable = true;
-
-    enableBashIntegration = false;
-    enableZshIntegration = true;
-    enableFishIntegration = false;
-
-    daemon = {
+  programs = {
+    atuin = {
       enable = true;
+
+      enableBashIntegration = false;
+      enableZshIntegration = true;
+      enableFishIntegration = false;
+
+      daemon = {
+        enable = true;
+        logLevel = "error";
+      };
+
+      settings = {
+        auto_sync = true;
+        sync_address = "https://atuin-sync.kempkens.io";
+        sync_frequency = "30m";
+        update_check = false;
+
+        style = "auto";
+        inline_height = 40;
+        search_mode = "fuzzy";
+        filter_mode_shell_up_key_binding = "directory";
+
+        history_filter = [
+          "^base64decode"
+          "^instagram-dl"
+          "^mp4concat"
+        ];
+      };
     };
 
-    settings = {
-      auto_sync = true;
-      sync_address = "https://atuin-sync.kempkens.io";
-      sync_frequency = "30m";
-      update_check = false;
-
-      style = "auto";
-      inline_height = 40;
-      search_mode = "fuzzy";
-      filter_mode_shell_up_key_binding = "directory";
-
-      history_filter = [
-        "^base64decode"
-        "^instagram-dl"
-        "^mp4concat"
-      ];
+    zsh.history = {
+      save = 0;
+      size = 0;
+      share = false;
     };
-  };
 
-  programs.zsh.history = {
-    save = 0;
-    size = 0;
-    share = false;
+    fish.shellInit = ''
+      set -x fish_history ""
+    '';
   };
-
-  programs.fish.shellInit = ''
-    set -x fish_history ""
-  '';
 }
