91966963a9645b9f2f365d2674859e2f32908e1a
Daniel Kempkens <daniel+git@kempkens.io>
2025-02-01 17:59:23 +0100

    nvim: sql highlight in sigils

--- home/config/nvim/after/queries/elixir/injections.scm
+++ home/config/nvim/after/queries/elixir/injections.scm
@@ -6,3 +6,10 @@
   (quoted_content) @injection.content
   (#eq? @_sigil_name "LUA")
   (#set! injection.language "lua"))
+
+; SQL
+(sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @injection.content
+  (#eq? @_sigil_name "SQL")
+  (#set! injection.language "sql"))
