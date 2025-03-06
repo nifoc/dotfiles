b262a48d6f4ffd20cc4d160e161124b68ecc0e4f
Daniel Kempkens <daniel+git@kempkens.io>
2025-03-06 08:47:09 +0100

    git: support mergiraf

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -54,6 +54,11 @@ in
         conflictStyle = "zdiff3";
       };
 
+      "merge \"mergiraf\"" = {
+        name = "mergiraf";
+        driver = "${lib.getExe pkgs.mergiraf} merge --git %O %A %B -s %S -x %X -y %Y -p %P -l %L";
+      };
+
       rebase = {
         updateRefs = true;
       };
@@ -91,6 +96,42 @@ in
       "*.swp"
       ".direnv/"
       ".DS_Store"
+    ];
+
+    attributes = [
+      "*.java merge=mergiraf"
+      "*.kt merge=mergiraf"
+      "*.rs merge=mergiraf"
+      "*.go merge=mergiraf"
+      "*.js merge=mergiraf"
+      "*.jsx merge=mergiraf"
+      "*.mjs merge=mergiraf"
+      "*.json merge=mergiraf"
+      "*.yml merge=mergiraf"
+      "*.yaml merge=mergiraf"
+      "*.toml merge=mergiraf"
+      "*.html merge=mergiraf"
+      "*.htm merge=mergiraf"
+      "*.xhtml merge=mergiraf"
+      "*.xml merge=mergiraf"
+      "*.c merge=mergiraf"
+      "*.h merge=mergiraf"
+      "*.cc merge=mergiraf"
+      "*.cpp merge=mergiraf"
+      "*.hpp merge=mergiraf"
+      "*.cs merge=mergiraf"
+      "*.dart merge=mergiraf"
+      "*.dts merge=mergiraf"
+      "*.scala merge=mergiraf"
+      "*.sbt merge=mergiraf"
+      "*.ts merge=mergiraf"
+      "*.tsx merge=mergiraf"
+      "*.py merge=mergiraf"
+      "*.php merge=mergiraf"
+      "*.phtml merge=mergiraf"
+      "*.sol merge=mergiraf"
+      "*.lua merge=mergiraf"
+      "*.rb merge=mergiraf"
     ];
 
     includes =
