e0b816fce7ac3b5eb5997eaba7cae6145f7231b7
Daniel Kempkens <daniel+git@kempkens.io>
2025-07-09 10:49:02 +0200

    iex: add version checks

--- home/programs/iex.nix
+++ home/programs/iex.nix
@@ -11,9 +11,15 @@ in
     ''
       Application.put_env(:elixir, :ansi_enabled, true)
 
-      IEx.configure(
-        colors: [enabled: true],
-        auto_reload: true
-      )
+      iex_options = [colors: [enabled: true]]
+
+      iex_options =
+        if Version.compare(System.version(), "1.18.0") in [:eq, :gt] do
+          iex_options ++ [auto_reload: true]
+        else
+          iex_options
+        end
+
+      IEx.configure(iex_options)
     '';
 }
