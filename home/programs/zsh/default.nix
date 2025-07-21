444b38902386e7e453c5aec9041ca3cac8509940
Daniel Kempkens <daniel+git@kempkens.io>
2025-07-21 20:16:04 +0200

    zsh: less settings

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -30,110 +30,109 @@ in
         ];
       };
 
-      plugins =
-        [
-          {
-            # https://github.com/Aloxaf/fzf-tab
-            name = "fzf-tab";
-            src = fetchFromGitHub {
-              owner = "Aloxaf";
-              repo = "fzf-tab";
-              tag = "v1.2.0";
-              hash = "sha256-q26XVS/LcyZPRqDNwKKA9exgBByE0muyuNb0Bbar2lY=";
-            };
-          }
+      plugins = [
+        {
+          # https://github.com/Aloxaf/fzf-tab
+          name = "fzf-tab";
+          src = fetchFromGitHub {
+            owner = "Aloxaf";
+            repo = "fzf-tab";
+            tag = "v1.2.0";
+            hash = "sha256-q26XVS/LcyZPRqDNwKKA9exgBByE0muyuNb0Bbar2lY=";
+          };
+        }
 
-          {
-            # https://github.com/Freed-Wu/fzf-tab-source
-            name = "fzf-tab-source";
-            src = fetchFromGitHub {
-              owner = "Freed-Wu";
-              repo = "fzf-tab-source";
-              rev = "a06c2cf1f9b4f1582cb7536ce06f12ba02696ea6";
-              hash = "sha256-0k6x4AhO8ULqanA+1bTNLhMGVz2A3K7LXQ/MgSLuQkc=";
-            };
-          }
+        {
+          # https://github.com/Freed-Wu/fzf-tab-source
+          name = "fzf-tab-source";
+          src = fetchFromGitHub {
+            owner = "Freed-Wu";
+            repo = "fzf-tab-source";
+            rev = "a06c2cf1f9b4f1582cb7536ce06f12ba02696ea6";
+            hash = "sha256-0k6x4AhO8ULqanA+1bTNLhMGVz2A3K7LXQ/MgSLuQkc=";
+          };
+        }
 
-          {
-            # https://github.com/hlissner/zsh-autopair
-            name = "zsh-autopair";
-            src = fetchFromGitHub {
-              owner = "hlissner";
-              repo = "zsh-autopair";
-              rev = "449a7c3d095bc8f3d78cf37b9549f8bb4c383f3d";
-              hash = "sha256-3zvOgIi+q7+sTXrT+r/4v98qjeiEL4Wh64rxBYnwJvQ=";
-            };
-          }
+        {
+          # https://github.com/hlissner/zsh-autopair
+          name = "zsh-autopair";
+          src = fetchFromGitHub {
+            owner = "hlissner";
+            repo = "zsh-autopair";
+            rev = "449a7c3d095bc8f3d78cf37b9549f8bb4c383f3d";
+            hash = "sha256-3zvOgIi+q7+sTXrT+r/4v98qjeiEL4Wh64rxBYnwJvQ=";
+          };
+        }
 
-          {
-            # https://github.com/Freed-Wu/zsh-colorize-functions
-            name = "zsh-colorize-functions";
-            src = fetchFromGitHub {
-              owner = "Freed-Wu";
-              repo = "zsh-colorize-functions";
-              rev = "46a299f8f8b1c71867679eaf4f67e30fd2746820";
-              hash = "sha256-DvqhWgox8v3Km1Rzkg7tEDqJ2zF62K6ILAzlOhUbuCc=";
-            };
-          }
+        {
+          # https://github.com/Freed-Wu/zsh-colorize-functions
+          name = "zsh-colorize-functions";
+          src = fetchFromGitHub {
+            owner = "Freed-Wu";
+            repo = "zsh-colorize-functions";
+            rev = "46a299f8f8b1c71867679eaf4f67e30fd2746820";
+            hash = "sha256-DvqhWgox8v3Km1Rzkg7tEDqJ2zF62K6ILAzlOhUbuCc=";
+          };
+        }
 
-          {
-            # https://github.com/Freed-Wu/zsh-help
-            name = "zsh-help";
-            src = fetchFromGitHub {
-              owner = "Freed-Wu";
-              repo = "zsh-help";
-              rev = "95cbc114078d8209730e38c72a6fa5859ca0773d";
-              hash = "sha256-ij+ooXQxV3CmsCN/CrJMicTWvS+9GYHA/1Kuqh5zXIY=";
-            };
-          }
+        {
+          # https://github.com/Freed-Wu/zsh-help
+          name = "zsh-help";
+          src = fetchFromGitHub {
+            owner = "Freed-Wu";
+            repo = "zsh-help";
+            rev = "95cbc114078d8209730e38c72a6fa5859ca0773d";
+            hash = "sha256-ij+ooXQxV3CmsCN/CrJMicTWvS+9GYHA/1Kuqh5zXIY=";
+          };
+        }
 
-          {
-            # https://github.com/dracula/zsh-syntax-highlighting
-            name = "dracula-zsh-syntax-highlighting";
-            file = "zsh-syntax-highlighting.sh";
-            src = fetchFromGitHub {
-              owner = "dracula";
-              repo = "zsh-syntax-highlighting";
-              rev = "09c89b657ad8a27ddfe1d6f2162e99e5cce0d5b3";
-              hash = "sha256-JrSKx8qHGAF0DnSJiuKWvn6ItQHvWpJ5pKo4yNbrHno=";
-            };
-          }
-        ]
-        ++ optionals (!builtins.hasAttr "nedeco" config || !config.nedeco.zsh.enable) [
-          {
-            # https://github.com/zsh-users/zsh-autosuggestions
-            name = "zsh-autosuggestions";
-            src = fetchFromGitHub {
-              owner = "zsh-users";
-              repo = "zsh-autosuggestions";
-              tag = "v0.7.1";
-              hash = "sha256-vpTyYq9ZgfgdDsWzjxVAE7FZH4MALMNZIFyEOBLm5Qo=";
-            };
-          }
+        {
+          # https://github.com/dracula/zsh-syntax-highlighting
+          name = "dracula-zsh-syntax-highlighting";
+          file = "zsh-syntax-highlighting.sh";
+          src = fetchFromGitHub {
+            owner = "dracula";
+            repo = "zsh-syntax-highlighting";
+            rev = "09c89b657ad8a27ddfe1d6f2162e99e5cce0d5b3";
+            hash = "sha256-JrSKx8qHGAF0DnSJiuKWvn6ItQHvWpJ5pKo4yNbrHno=";
+          };
+        }
+      ]
+      ++ optionals (!builtins.hasAttr "nedeco" config || !config.nedeco.zsh.enable) [
+        {
+          # https://github.com/zsh-users/zsh-autosuggestions
+          name = "zsh-autosuggestions";
+          src = fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-autosuggestions";
+            tag = "v0.7.1";
+            hash = "sha256-vpTyYq9ZgfgdDsWzjxVAE7FZH4MALMNZIFyEOBLm5Qo=";
+          };
+        }
 
-          {
-            # https://github.com/trystan2k/zsh-tab-title
-            name = "zsh-tab-title";
-            src = fetchFromGitHub {
-              owner = "trystan2k";
-              repo = "zsh-tab-title";
-              tag = "v3.1.0";
-              hash = "sha256-YvAN8c2++WRJYGblstZKWCWrCl0byXQqFryTA35/Ao0=";
-            };
-          }
-        ]
-        ++ optionals isDarwin [
-          {
-            # https://github.com/MichaelAquilina/zsh-auto-notify
-            name = "zsh-auto-notify";
-            src = fetchFromGitHub {
-              owner = "MichaelAquilina";
-              repo = "zsh-auto-notify";
-              tag = "0.11.0";
-              hash = "sha256-8r5RsyldJIzlWr9+G8lrkHvJ8KxTVO859M//wDnYOUY=";
-            };
-          }
-        ];
+        {
+          # https://github.com/trystan2k/zsh-tab-title
+          name = "zsh-tab-title";
+          src = fetchFromGitHub {
+            owner = "trystan2k";
+            repo = "zsh-tab-title";
+            tag = "v3.1.0";
+            hash = "sha256-YvAN8c2++WRJYGblstZKWCWrCl0byXQqFryTA35/Ao0=";
+          };
+        }
+      ]
+      ++ optionals isDarwin [
+        {
+          # https://github.com/MichaelAquilina/zsh-auto-notify
+          name = "zsh-auto-notify";
+          src = fetchFromGitHub {
+            owner = "MichaelAquilina";
+            repo = "zsh-auto-notify";
+            tag = "0.11.0";
+            hash = "sha256-8r5RsyldJIzlWr9+G8lrkHvJ8KxTVO859M//wDnYOUY=";
+          };
+        }
+      ];
 
       shellAliases =
         let
@@ -213,6 +212,8 @@ in
                 path=("$HOME/.bin" $path)
                 export PATH
               fi
+
+              export LESS="--quit-if-one-screen --ignore-case --RAW-CONTROL-CHARS"
             ''
           )
         ]
