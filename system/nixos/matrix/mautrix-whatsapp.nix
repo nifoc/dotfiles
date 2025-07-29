c7d9131d3918faa2ea821302f585ef3f70be96ab
Daniel Kempkens <daniel+git@kempkens.io>
2025-07-29 15:56:45 +0200

    mautrix-whatsapp: update configuration

--- system/nixos/matrix/mautrix-whatsapp.nix
+++ system/nixos/matrix/mautrix-whatsapp.nix
@@ -6,41 +6,42 @@
     serviceDependencies = [ "continuwuity.service" ];
 
     environmentFile = config.age.secrets.mautrix-whatsapp.path;
-    # Settings have to be written in the old format: https://github.com/mautrix/whatsapp/blob/v0.10.9/example-config.yaml
-    # Update once this is merged: https://github.com/NixOS/nixpkgs/pull/350448
     settings = {
       backfill = {
         enabled = true;
       };
 
       bridge = {
-        message_status_events = true;
-
-        encryption = {
-          allow = true;
-          default = true;
-        };
-
-        history_sync.backfill = true;
-
-        login_shared_secret_map = {
-          "kempkens.io" = "$DOUBLE_PUPPET_SECRETS_HOMESERVER";
-        };
-
         permissions = {
           "*" = "relay";
           "kempkens.io" = "user";
           "@daniel:kempkens.io" = "admin";
         };
+      };
 
-        provisioning = {
-          shared_secret = "$PROVISIONING_SHARED_SECRET";
+      double_puppet = {
+        secrets = {
+          "kempkens.io" = "$DOUBLE_PUPPET_SECRETS_HOMESERVER";
         };
+      };
+
+      encryption = {
+        allow = true;
+        default = true;
+        pickle_key = "$ENCRYPTION_PICKLE_KEY";
       };
 
       homeserver = {
         address = "http://127.0.0.1:6167";
         domain = "kempkens.io";
+      };
+
+      matrix = {
+        message_status_events = true;
+      };
+
+      provisioning = {
+        shared_secret = "$PROVISIONING_SHARED_SECRET";
       };
     };
   };
