186bf7ccb18903074a406f438918fc35c220c836
Daniel Kempkens <daniel+git@kempkens.io>
2025-08-09 23:26:00 +0200

    flake: update lix-module

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/release-2.93.tar.gz";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         lix.follows = "lix";
