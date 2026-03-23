fc8b3e6c685cb3678f120e6474bdb52197777b42
Daniel Kempkens <daniel+git@kempkens.io>
2026-03-23 10:54:59 +0100

    sudo: enable feedback

--- system/darwin/sudo.nix
+++ system/darwin/sudo.nix
@@ -1,6 +1,12 @@
 {
-  security.pam.services.sudo_local = {
-    touchIdAuth = true;
-    watchIdAuth = true;
+  security = {
+    pam.services.sudo_local = {
+      touchIdAuth = true;
+      watchIdAuth = true;
+    };
+
+    sudo.extraConfig = ''
+      Defaults pwfeedback
+    '';
   };
 }
