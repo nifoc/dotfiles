379fcd4b40235331518f9128c6d21854f1b8344a
Daniel Kempkens <daniel+git@kempkens.io>
2026-09-01 22:07:49 +0200

    feat(git): add different git ssh url format

--- modules/private-device/git.nix
+++ modules/private-device/git.nix
@@ -1,51 +1,44 @@
 {
   den.aspects.private-device = {
     homeManager = {
-      programs.git = {
-        includes = [
-          {
-            condition = "hasconfig:remote.*.url:forgejo@git.kempkens.io:*/**";
-            contents = {
-              user = {
-                signingKey = "~/.ssh/GitHub.pub";
-              };
-
-              commit = {
-                gpgSign = true;
-              };
-
-              tag = {
-                gpgSign = true;
-              };
-
-              gpg = {
-                format = "ssh";
-              };
+      programs.git =
+        let
+          privateConfig = {
+            user = {
+              signingKey = "~/.ssh/GitHub.pub";
             };
-          }
 
-          {
-            condition = "hasconfig:remote.*.url:git@github.com:*/**";
-            contents = {
-              user = {
-                signingKey = "~/.ssh/GitHub.pub";
-              };
-
-              commit = {
-                gpgSign = true;
-              };
-
-              tag = {
-                gpgSign = true;
-              };
-
-              gpg = {
-                format = "ssh";
-              };
+            commit = {
+              gpgSign = true;
             };
-          }
-        ];
-      };
+
+            tag = {
+              gpgSign = true;
+            };
+
+            gpg = {
+              format = "ssh";
+            };
+          };
+        in
+        {
+          includes = [
+            {
+              condition = "hasconfig:remote.*.url:forgejo@git.kempkens.io:*/**";
+              contents = privateConfig;
+            }
+
+            {
+              condition = "hasconfig:remote.*.url:ssh://forgejo@git.kempkens.io/**";
+              contents = privateConfig;
+            }
+
+            {
+              condition = "hasconfig:remote.*.url:git@github.com:*/**";
+              contents = privateConfig;
+            }
+          ];
+        };
     };
   };
 }
