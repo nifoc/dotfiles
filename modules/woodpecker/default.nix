5753993de632ff7b055a2a938f01e84a17d4e95f
Daniel Kempkens <daniel+git@kempkens.io>
2026-09-02 23:50:37 +0200

    chore(ci): disable version check

--- modules/woodpecker/default.nix
+++ modules/woodpecker/default.nix
@@ -44,6 +44,7 @@
               WOODPECKER_PLUGINS_TRUSTED_CLONE = "docker.io/woodpeckerci/plugin-git,quay.io/woodpeckerci/plugin-git,bash";
 
               WOODPECKER_ENABLE_SWAGGER = "false";
+              WOODPECKER_DISABLE_VERSION_CHECK = "true";
               WOODPECKER_DISABLE_USER_AGENT_REGISTRATION = "true";
             };
             environmentFile = [ config.age.secrets.woodpecker-environment.path ];
