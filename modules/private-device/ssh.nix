658f79a552a62d2c88d25a44771ca919df2cf444
Daniel Kempkens <daniel+git@kempkens.io>
2026-09-13 01:39:39 +0200

    feat(ssh): simpler configuration

--- modules/private-device/ssh.nix
+++ modules/private-device/ssh.nix
@@ -1,183 +1,112 @@
 {
   den.aspects.private-device = {
-    homeManager = {
-      programs.ssh = {
-        settings = {
-          "Host github.com" = {
-            Port = 22;
-            User = "git";
-            IdentityFile = "~/.ssh/GitHub.pub";
-            IdentitiesOnly = true;
-            Compression = true;
-          };
+    homeManager =
+      { pkgs, lib, ... }:
+      {
+        programs.ssh =
+          let
+            lan = name: localIP: {
+              "Host ${name}" = {
+                Port = 22;
+                User = "daniel";
+                ForwardAgent = true;
+                IdentityFile = "~/.ssh/LAN.pub";
+                IdentitiesOnly = true;
+              };
 
-          "Host gitlab.com" = {
-            Port = 22;
-            User = "git";
-            IdentityFile = "~/.ssh/GitLab.pub";
-            IdentitiesOnly = true;
-            Compression = true;
-          };
+              "Host ${name}-unlock" = {
+                Hostname = localIP;
+                Port = 2222;
+                User = "root";
+                IdentityFile = "~/.ssh/LAN.pub";
+                IdentitiesOnly = true;
+              };
 
-          "Host git.kempkens.io" = {
-            Port = 22;
-            User = "forgejo";
-            IdentityFile = "~/.ssh/GitHub.pub";
-            IdentitiesOnly = true;
-            Compression = true;
-          };
+              "${name}-match" = lib.hm.dag.entryAfter [ "Host ${name}" ] {
+                header = "Match host ${name} exec \"nc -w 1 -z ${localIP} %p\"";
+                Hostname = "${name}.lan";
+              };
 
-          "Host router" = {
-            Hostname = "10.0.0.1";
-            Port = 22;
-            User = "root";
-            ForwardAgent = true;
-            PreferredAuthentications = "keyboard-interactive";
-            SetEnv = {
-              TERM = "xterm-256color";
+              "${name}-fallback" = lib.hm.dag.entryAfter [ "${name}-match" ] {
+                header = "Match host ${name}";
+                Hostname = "${name}.ts.kempkens.network";
+              };
             };
-          };
 
-          "Host nas" = {
-            Hostname = "10.0.0.100";
-            Port = 22;
-            User = "daniel";
-            IdentityFile = "~/.ssh/LAN.pub";
-            IdentitiesOnly = true;
-            SetEnv = {
-              TERM = "xterm-256color";
+            vps = name: remoteIP: {
+              "Host ${name}" = {
+                Hostname = "${name}.ts.kempkens.network";
+                Port = 22;
+                User = "daniel";
+                IdentityFile = "~/.ssh/Hetzner.pub";
+                IdentitiesOnly = true;
+              };
+
+              "Host ${name}-unlock" = {
+                Hostname = remoteIP;
+                Port = 2222;
+                User = "root";
+                IdentityFile = "~/.ssh/Hetzner.pub";
+                IdentitiesOnly = true;
+              };
             };
+          in
+          {
+            settings = {
+              "Host github.com" = {
+                Port = 22;
+                User = "git";
+                IdentityFile = "~/.ssh/GitHub.pub";
+                IdentitiesOnly = true;
+                Compression = true;
+              };
+
+              "Host gitlab.com" = {
+                Port = 22;
+                User = "git";
+                IdentityFile = "~/.ssh/GitLab.pub";
+                IdentitiesOnly = true;
+                Compression = true;
+              };
+
+              "Host git.kempkens.io" = {
+                Port = 22;
+                User = "forgejo";
+                IdentityFile = "~/.ssh/GitHub.pub";
+                IdentitiesOnly = true;
+                Compression = true;
+              };
+
+              "Host router" = {
+                Hostname = "10.0.0.1";
+                Port = 22;
+                User = "daniel";
+                IdentityFile = "~/.ssh/LAN.pub";
+                IdentitiesOnly = true;
+                SetEnv = {
+                  TERM = "xterm-256color";
+                };
+              };
+
+              "Host adsb-antenna" = {
+                Hostname = "adsb-antenna.laniot";
+                Port = 22;
+                User = "daniel";
+                ForwardAgent = true;
+                IdentityFile = "~/.ssh/LAN.pub";
+                IdentitiesOnly = true;
+              };
+            }
+            // (lan "argon" "10.0.0.5")
+            // (lan "krypton" "10.0.0.100")
+            // (lan "neon" "10.0.0.7")
+            // (lan "xenon" "10.0.0.101")
+            // (vps "boron" "138.199.219.102")
+            // (vps "carbon" "49.12.228.29")
+            // (vps "gallium" "51.89.118.129");
           };
 
-          "Host adsb-antenna" = {
-            Hostname = "adsb-antenna.laniot";
-            Port = 22;
-            User = "daniel";
-            ForwardAgent = true;
-            IdentityFile = "~/.ssh/LAN.pub";
-            IdentitiesOnly = true;
-          };
-
-          "Host carbon" = {
-            Hostname = "carbon.ts.kempkens.network";
-            Port = 22;
-            User = "daniel";
-            ForwardAgent = true;
-            IdentityFile = "~/.ssh/Hetzner.pub";
-            IdentitiesOnly = true;
-          };
-
-          "Host carbon-unlock" = {
-            Hostname = "49.12.228.29";
-            Port = 2222;
-            User = "root";
-            IdentityFile = "~/.ssh/Hetzner.pub";
-            IdentitiesOnly = true;
-          };
-
-          "Host boron" = {
-            Hostname = "boron.ts.kempkens.network";
-            Port = 22;
-            User = "daniel";
-            ForwardAgent = true;
-            IdentityFile = "~/.ssh/Hetzner.pub";
-            IdentitiesOnly = true;
-          };
-
-          "Host boron-unlock" = {
-            Hostname = "138.199.219.102";
-            Port = 2222;
-            User = "root";
-            IdentityFile = "~/.ssh/Hetzner.pub";
-            IdentitiesOnly = true;
-          };
-
-          "Host gallium" = {
-            Hostname = "51.89.118.129";
-            Port = 22;
-            User = "daniel";
-            ForwardAgent = true;
-            IdentityFile = "~/.ssh/Hetzner.pub";
-            IdentitiesOnly = true;
-          };
-
-          "Host gallium-unlock" = {
-            Hostname = "51.89.118.129";
-            Port = 2222;
-            User = "root";
-            IdentityFile = "~/.ssh/Hetzner.pub";
-            IdentitiesOnly = true;
-          };
-
-          "Host krypton" = {
-            Hostname = "krypton.ts.kempkens.network";
-            Port = 22;
-            User = "daniel";
-            ForwardAgent = true;
-            IdentityFile = "~/.ssh/LAN.pub";
-            IdentitiesOnly = true;
-          };
-
-          "Host krypton-unlock" = {
-            Hostname = "10.0.0.100";
-            Port = 2222;
-            User = "root";
-            IdentityFile = "~/.ssh/LAN.pub";
-            IdentitiesOnly = true;
-          };
-
-          "Host xenon" = {
-            Hostname = "xenon.ts.kempkens.network";
-            Port = 22;
-            User = "daniel";
-            ForwardAgent = true;
-            IdentityFile = "~/.ssh/LAN.pub";
-            IdentitiesOnly = true;
-          };
-
-          "Host xenon-unlock" = {
-            Hostname = "10.0.0.101";
-            Port = 2222;
-            User = "root";
-            IdentityFile = "~/.ssh/LAN.pub";
-            IdentitiesOnly = true;
-          };
-
-          "Host argon" = {
-            Hostname = "argon.ts.kempkens.network";
-            Port = 22;
-            User = "daniel";
-            ForwardAgent = true;
-            IdentityFile = "~/.ssh/LAN.pub";
-            IdentitiesOnly = true;
-          };
-
-          "Host argon-unlock" = {
-            Hostname = "10.0.0.5";
-            Port = 2222;
-            User = "root";
-            IdentityFile = "~/.ssh/LAN.pub";
-            IdentitiesOnly = true;
-          };
-
-          "Host neon" = {
-            Hostname = "neon.ts.kempkens.network";
-            Port = 22;
-            User = "daniel";
-            ForwardAgent = true;
-            IdentityFile = "~/.ssh/LAN.pub";
-            IdentitiesOnly = true;
-          };
-
-          "Host neon-unlock" = {
-            Hostname = "10.0.0.7";
-            Port = 2222;
-            User = "root";
-            IdentityFile = "~/.ssh/LAN.pub";
-            IdentitiesOnly = true;
-          };
-        };
+        home.packages = with pkgs; [ netcat ];
       };
-    };
   };
 }
